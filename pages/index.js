05110ca2607d6b89839160929b59c7758dd4d41c
FelipeZdog <felipematheusti@gmail.com>
2023-04-30 15:47:19 -0300

    alterei favicon/titulo

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,16 @@ import Banner from "../components/Banner";
 import Especialistas from "../components/Especialistas";
 import Grupo from "../components/Grupo";
 import Pioneiros from "../components/Pioneiros";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
+      <Head>
+        <title>Clínica Rennovi</title>
+        <meta name="description" content="by medusa" />
+        <link rel="icon" href="/logos/renovi_white.png" />
+      </Head>
       <Banner />
       <Pioneiros />
       <Grupo />

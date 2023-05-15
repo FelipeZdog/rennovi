970843ecc5a616f4e05a896d57220c7af475a9d3
FelipeZdog <felipematheusti@gmail.com>
2023-05-15 11:23:05 -0300

    alteração 2

--- components/Banner.js
+++ components/Banner.js
@@ -5,17 +5,17 @@ import Link from "next/link";
 function Banner() {
   return (
     <div
-      className="flex justify-center items-center lg:justify-start lg:items-start banner-web h-full text-white font-poppins text-center lg:text-left centered-content w-full sm:p-20 md:p-24 lg:p-[10%]"
+      className="flex justify-center items-center lg:justify-start lg:items-start banner-web h-[30vh] p-10 sm:h-[40vh] lg:h-full text-white font-poppins text-center lg:text-left centered-content w-full sm:p-10 md:p-[10%] lg:p-[10%]"
       id="inicio"
     >
       <div className="flex flex-col centered-content">
-        <h1 className="sm:text-[20px] md:text[30px] lg:text-[40px] 2x1:text-[50px] py-0">
+        <h1 className="text-[20px] sm:text-[20px] md:text[30px] lg:text-[40px] 2x1:text-[50px] py-0">
           Conheça a <span className="font-extrabold">Clínica Rennovi</span>®!
         </h1>
-        <p className="mt-2 text-xs sm:text-lg">
+        <p className="mt-2 text-[15px] sm:text-lg md:text-[15px] lg:text-[20px]">
           A primeira clínica boutique da região metropolitana de Fortaleza.
         </p>
-        <p className="mt-2 text-xs sm:text-lg">
+        <p className="mt-2 text-[15px] sm:text-lg  md:text-[15px] lg:text-[20px]">
           Somos o maior time de especialistas com as maiores tecnologias no
           cuidado da saúde, beleza e bem estar.
         </p>
